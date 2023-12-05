02a9ede97c6c5f2651a44ffb4443553cce1a7352
albertbuh <albertbuhovets@mail.ru>
2023-12-05 17:44:32 +0300

    update tests

--- test/Library.API.Tests/LibraryServiceTests.cs
+++ test/Library.API.Tests/LibraryServiceTests.cs
@@ -6,14 +6,58 @@ public class LibraryServiceTests
 {
   private ILibraryService service;
   private ITestOutputHelper output;
+
+  private readonly BookEdition testBookEdition;
+
   public LibraryServiceTests(ITestOutputHelper helper)
   {
     service = new LibraryService();
     output = helper;
+
+    testBookEdition = new BookEdition(
+      "987-1-234-56789-0",
+      "bookTitle",
+      "Some description with кириллица"
+    );
+    testBookEdition.Authors = new List<Author>()
+    {
+      new Author("Пушкин", "Александр", "Сергеевич"),
+      new Author("Достоевский", "Фёдор")
+    };
+    testBookEdition.Genres = new List<Genre>() { new Genre("комедия"), new Genre("триллер") };
+  }
+
+
+  [Fact]
+  public async void CreateUpdateAndDeleteTestBookEdition() 
+  { 
+    const string newDesc = "описание на кириллице";
+    
+    var bookEdition = await service.GetBookByISBN(testBookEdition.ISBN);
+    if(bookEdition == null)
+    {
+      await service.AddBookEdition(testBookEdition);
+      bookEdition = await service.GetBookByISBN(testBookEdition.ISBN);
+    }
+    
+    Assert.NotNull(bookEdition);
+
+    bookEdition.Description = newDesc;
+
+    await service.UpdateBookEdition(testBookEdition.ISBN, bookEdition);
+    bookEdition = await service.GetBookByISBN(testBookEdition.ISBN);
+
+    Assert.NotNull(bookEdition);
+    Assert.Equal(bookEdition.Description, newDesc);
+
+    await service.DeleteBookEdition(testBookEdition.ISBN);
+    bookEdition = await service.GetBookByISBN(testBookEdition.ISBN);
+
+    Assert.Null(bookEdition);
   }
 
   [Fact]
-  public async void GetBookEditionByISBN()
+  public async void GetBookEditionByISBNAndCheckIncludeEntities()
   {
     var bookEdition = await service.GetBookByISBN("978-5-04-111308-7");
     Assert.NotNull(bookEdition);
@@ -46,6 +90,31 @@ public class LibraryServiceTests
     }
   }
 
+  [Fact]
+  public async void UpdateBookInstanceWithMinimumIdByChangingTakenDate()
+  {
+    int curId = 0;
+    BookInstance? bookInstance;
+    do
+    {
+      curId++;
+      bookInstance = await service.GetBookInstanceById(curId);
+    } while (bookInstance == null && curId < 30);
+
+    Assert.NotNull(bookInstance);
+
+    BookInstance newInfo = new BookInstance(bookInstance.Book);
+    newInfo.DateOfTaken = DateOnly.FromDateTime(DateTime.Now);
+    newInfo.DateOfReturn = newInfo.DateOfTaken.Value.AddDays(7);
+    await service.UpdateBookInstance(curId, newInfo);
+
+    var newBookInstanceFromContext = await service.GetBookInstanceById(curId);
+
+    Assert.NotNull(newBookInstanceFromContext);
+    Assert.NotNull(newBookInstanceFromContext.DateOfTaken);
+    Assert.NotNull(newBookInstanceFromContext.DateOfReturn);
+  }
+
   [Fact(Skip = "one-time test to verify that addition of book instances to db is correct")]
   public void AddBookInstances()
   {
@@ -65,23 +134,7 @@ public class LibraryServiceTests
       serviceRequestCount++;
     } while (bookInstance == null && serviceRequestCount < 20);
 
-    service.DeleteBookInstance(minId);
+    await service.DeleteBookInstance(minId);
     Assert.NotNull(bookInstance);
-  }
-
-  [Fact(Skip = "no need at the moment")]
-  public void CreateNewBookEdition()
-  {
-    BookEdition bookInfo = new BookEdition();
-    bookInfo.Title = "TestBook";
-    bookInfo.ISBN = "987-1-234-56789-0";
-    bookInfo.Description = "Some description with кириллица";
-    bookInfo.Authors = new List<Author>()
-    {
-      new Author("Пушкин", "Александр", "Сергеевич"),
-      new Author("Достоевский", "Фёдор")
-    };
-    bookInfo.Genres = new List<Genre>() { new Genre("комедия"), new Genre("триллер") };
-    service.AddBookEdition(bookInfo);
   }
 }
