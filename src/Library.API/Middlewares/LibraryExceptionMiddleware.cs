1d1fbe1e8255767a3be3b2803015c710611c348c
albertbuh <albertbuhovets@mail.ru>
2023-12-19 18:13:34 +0300

    exception handling

---
+++ src/Library.API/Middlewares/LibraryExceptionMiddleware.cs
@@ -0,0 +1,31 @@
+// using Library.API.Services.Exceptions;
+namespace Library.API.Middlewares;
+
+public class LibraryExceptionMiddleware
+{
+  private static readonly ILogger logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger("LibraryExceptionMiddleware");
+  
+  private readonly RequestDelegate _next;
+
+  public LibraryExceptionMiddleware(RequestDelegate next)
+  {
+    _next = next;
+  }
+
+  public async Task Invoke(HttpContext context)
+  {
+    try
+    {
+      await _next.Invoke(context);
+    }
+    catch(Exception e)
+    {
+      logger.LogError(e, $"error in processing {context.Request.Path.Value}");
+      var response = context.Response;
+      response.ContentType = "text/html";
+      response.StatusCode = StatusCodes.Status400BadRequest;
+      await response.WriteAsync(e.Message);
+    }
+  }
+
+}
